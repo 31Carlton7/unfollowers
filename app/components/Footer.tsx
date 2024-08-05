54f48675bce9bd8864f7adaa5a7a06015adea7d4
Carlton Aikins <carlton@charmtechnologies.co>
2024-08-05 03:22:11 -0400

    Create a footer to display copyright

---
+++ app/components/Footer.tsx
@@ -0,0 +1,26 @@
+/**
+ * This component renders a footer component with copyright information and an Instagram link.
+ */
+
+import React from 'react';
+import { FaInstagram } from 'react-icons/fa';
+
+/**
+ * Renders a footer component with copyright information and an Instagram link.
+ *
+ * @return {JSX.Element} The rendered footer component.
+ */
+const Footer = (): JSX.Element => {
+  return (
+    <div className='flex w-full justify-between'>
+      <p className='text-[#222222] font-bold text-xl mx-8 my-4'>© Charm Technologies 2024</p>
+      <div className='flex'>
+        <a href='https://www.instagram.com/31carlton7' target='_blank'>
+          <FaInstagram className='text-[#222222] font-bold text-xl mx-8 my-4' />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
