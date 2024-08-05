1a2ea773a8c8c9bca4e6261009747e75af31cb3b
Carlton Aikins <carlton@charmtechnologies.co>
2024-08-05 03:22:43 -0400

    Create a classname modifier

---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * This file contains utility functions.
+ */
+
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Merges and concatenates class names using the `clsx` and `twMerge` functions.
+ *
+ * @param {ClassValue[]} inputs - An array of class values to be merged and concatenated.
+ * @return {string} - The merged and concatenated class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
